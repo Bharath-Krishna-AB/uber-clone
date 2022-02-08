d4d7739098c4876234998934b5ef544555a3ac17
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-08 21:49:02 +0530

    fetched cordinates with location with api of mapbox

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,15 +1,38 @@
 
-import React from 'react';
+import {useEffect} from 'react';
 import tw from "tailwind-styled-components"
 import Map from './components/Map';
 
 
 const Confirm = () => {
+
+
+    const getCoordinates = ()=>{
+        const location ='Kochi';
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?`+
+        new URLSearchParams({
+            access_token: 'pk.eyJ1IjoiYmhhcmF0aC1hYiIsImEiOiJja3pjeGhxb2wxdXZrMzJtbTdwZ2ZkNGFmIn0.UKY16DAHjPQ4ydPP5eksYA'
+        })
+        )
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data);
+        })
+    }
+
+    useEffect(() => {
+        getCoordinates()
+
+    },[]);
+    
+
+
   return (
       <Wrapper>
           <Map/>
           <RideContainer>
-              {/* rideSelector */}
+              {/* rideSelector
+               */}
               {/* confirm btn */}
           </RideContainer>
       </Wrapper>
