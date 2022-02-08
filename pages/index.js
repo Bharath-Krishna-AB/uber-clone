ead091bbb7538833e74af52bb681c1434f48037f
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-08 09:58:35 +0530

    implemented mapbox to project

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import styles from '../styles/Home.module.css'
 import tw from "tailwind-styled-components"
 import mapboxgl from '!mapbox-gl'
+import { useEffect } from 'react';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhcmF0aC1hYiIsImEiOiJja3pjeGhxb2wxdXZrMzJtbTdwZ2ZkNGFmIn0.UKY16DAHjPQ4ydPP5eksYA';
 
 export default function Home() {
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-99.29011,39.39172],
+      zoom: 3
+    })
+  });
+
+  // const map = new mapboxgl.Map({
+  //   container: 'map',
+  //   style: 'mapbox://styles/mapbox/streets-v11'
+  // })
+
   return (
     <Wrapper>
-      <Map>Map</Map>
+      <Map id='map'></Map>
       <ActionItems>Start</ActionItems>
     </Wrapper>
   )
