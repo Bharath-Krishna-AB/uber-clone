bfce3d0503504ea10456445057edca7a5f761fed
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-08 22:01:59 +0530

    fetched pickup and dropoff coordinates

--- pages/confirm.js
+++ pages/confirm.js
@@ -7,22 +7,39 @@ import Map from './components/Map';
 const Confirm = () => {
 
 
-    const getCoordinates = ()=>{
-        const location ='Kochi';
-        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?`+
+    const getPickUpCoordinates = ()=>{
+        const pickUp ='Kochi';
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickUp}.json?`+
         new URLSearchParams({
-            access_token: 'pk.eyJ1IjoiYmhhcmF0aC1hYiIsImEiOiJja3pjeGhxb2wxdXZrMzJtbTdwZ2ZkNGFmIn0.UKY16DAHjPQ4ydPP5eksYA'
+            access_token: 'pk.eyJ1IjoiYmhhcmF0aC1hYiIsImEiOiJja3pjeGhxb2wxdXZrMzJtbTdwZ2ZkNGFmIn0.UKY16DAHjPQ4ydPP5eksYA',
+            limit: 1
         })
         )
         .then(response => response.json())
         .then(data =>{
-            console.log(data);
+            console.log('pickUp');
+            console.log(data.features[0].center);
+        })
+    }
+
+    const getDropOffCoordinates = ()=>{
+        const dropOff ='Thrissur';
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropOff}.json?`+
+        new URLSearchParams({
+            access_token: 'pk.eyJ1IjoiYmhhcmF0aC1hYiIsImEiOiJja3pjeGhxb2wxdXZrMzJtbTdwZ2ZkNGFmIn0.UKY16DAHjPQ4ydPP5eksYA',
+            limit: 1
+        })
+        )
+        .then(response => response.json())
+        .then(data =>{
+            console.log('dropOff');
+            console.log(data.features[0].center);
         })
     }
 
     useEffect(() => {
-        getCoordinates()
-
+        getPickUpCoordinates()
+        getDropOffCoordinates()
     },[]);
     
 
