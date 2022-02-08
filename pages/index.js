a1b31f04ff484500cfde4a7cff01119c067caff3
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-08 10:13:29 +0530

    created map.js in components

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,6 @@ export default function Home() {
     })
   });
 
-  // const map = new mapboxgl.Map({
-  //   container: 'map',
-  //   style: 'mapbox://styles/mapbox/streets-v11'
-  // })
-
   return (
     <Wrapper>
       <Map id='map'></Map>
