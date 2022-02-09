a731f7c2fc30b474b3ed14c62e7bf21c13b8108f
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-09 12:36:41 +0530

    added Carlist.js to loopthrough cars

---
+++ pages/data/CarList.js
@@ -0,0 +1,28 @@
+export const carList = [
+    {
+      imgUrl: '/Assets/uberx.png',
+      service: 'UberX',
+      multiplier: 1,
+    },
+    {
+      imgUrl: '/Assets/uberxl.png',
+      service: 'UberXL',
+      multiplier: 1.5,
+    },
+    {
+      imgUrl: '/Assets/uberblack.png',
+      service: 'Black',
+      multiplier: 2,
+    },
+    {
+      imgUrl: '/Assets/uberx.png',
+      service: 'Comfort',
+      multiplier: 1.2,
+    },
+    {
+      imgUrl: '/Assets/uberblacksuv.png',
+      service: 'Black SUV',
+      multiplier: 2.8,
+    }
+  ]
+  
