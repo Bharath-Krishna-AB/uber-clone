bfaaae4477f8752ee50d2fa807835632a562b335
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-09 20:43:26 +0530

    completed uberlogo in login page

--- pages/login.js
+++ pages/login.js
@@ -5,6 +5,7 @@ import tw from "tailwind-styled-components"
 const Login = () => {
   return (
     <Wrapper>
+        <UberLogo src="/Assets/uber-Logo-2.png"/>
         <LoginButton>Login in with Google</LoginButton>
     </Wrapper>
   )
@@ -18,4 +19,8 @@ flex flex-col h-screen bg-gray-200 p-4
 
 const LoginButton = tw.button`
 bg-black text-white  text-center py-4 mt-8 self-center w-full 
+`
+
+const UberLogo = tw.img`
+h-7 w-auto object-contain self-start
 `
