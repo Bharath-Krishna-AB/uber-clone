874b0055dc410df9c123665334a5483ba5b9e357
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-09 11:42:50 +0530

    added a sample html for Car section

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -7,7 +7,18 @@ const RideSelector = () => {
       <Wrapper>
           <Title>Choose a ride, or swipe up for more</Title>
           <CarList>
-
+              <Car>
+                  <CarImage src="/Assets/uberx.png"/>
+                  <CarDetails>
+                      <Service>
+                          UberX
+                      </Service>
+                      <Time>
+                          5 min away
+                      </Time>
+                  </CarDetails>
+                  <Price>$10.00</Price>
+              </Car>
           </CarList>
       </Wrapper>
   )
@@ -24,5 +35,28 @@ text-gray-500 text-center text-xs py-2 border-b
 `
 
 const CarList = tw.div`
+
+`
+const Car = tw.div`
+
+`
+
+const CarImage = tw.img`
+
+`
+
+const CarDetails = tw.div`
+
+`
+
+const Service = tw.div`
+
+`
+
+const Time = tw.div`
+
+`
+
+const Price = tw.div`
 
 `
