5ca5b04780c0ffdc2e2432bb02ad130ee9222a23
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-09 22:17:37 +0530

    import all things to signout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import styles from '../styles/Home.module.css'
 import tw from "tailwind-styled-components"
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import Map from './components/Map';
 import Link from 'next/link'
-
+import auth from '../firebase'
+import {onAuthStateChanged,signOut} from 'firebase/auth'
+import { useRouter } from 'next/router'
 
 export default function Home() {
 
