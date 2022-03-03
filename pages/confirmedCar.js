b9e0c7caac13d1d37ec3de859612213906f96c3a
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-03-03 10:50:47 +0530

    designed confirmedCar page

--- pages/confirmedCar.js
+++ pages/confirmedCar.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import tw from "tailwind-styled-components"
+
 
 
 const confirmedCar = () => {
   return (
-    <div>
-        Confirmed
-    </div>
+    <ConfirmedCar>
+        This is only a sample website <br/> so the car will not come .
+    </ConfirmedCar>
   )
 }
 
-export default confirmedCar
+export default confirmedCar;
+
+const ConfirmedCar = tw.div`
+flex h-screen flex-col bg-gray-200 text-center justify-center text-2xl font-bold text-blue-400
+`
